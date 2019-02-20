613fefb37a36d8c29328ca7dbd24bc01f20dc279
iryna_voitova <iryna_voitova@epam.com>
2019-02-20 22:18:31 +0200

    homeTaskOne

---
+++ java/project/src/com/epam/example/Main.java
@@ -0,0 +1,17 @@
+package com.epam.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("**** ****     *      *     * ");
+	    System.out.println("*    *  *    * *     * * * * ");
+	    System.out.println("**   ****   *   *    *  *  * ");
+	    System.out.println("*    *     * * * *   *     * ");
+	    System.out.println("**** *    *       *  *     *");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+}
